95cd5277b965e101ddfbc3b7951e81f99fcd36f8
Long9725 <develop9725@gmail.com>
2023-05-15 01:30:38 +0900

    <CHORE> Equatable

--- client/lib/domains/doit/presentation/cubits/celebrity_cubic/celebrity_state.dart
+++ client/lib/domains/doit/presentation/cubits/celebrity_cubic/celebrity_state.dart
@@ -1,7 +1,8 @@
 part of 'celebrity_cubic.dart';
 
-abstract class CelebrityCubicState extends Equatable {}
-
-class CelebrityCubicInit extends CelebrityCubicState {
-
+abstract class CelebrityCubicState extends Equatable {
+  @override
+  List<Object?> get props => [runtimeType];
 }
+
+class CelebrityCubicInit extends CelebrityCubicState {}
