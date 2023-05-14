c04949d1bfb1c3a89c48bb3e32320a4f948f2454
Long9725 <develop9725@gmail.com>
2023-05-15 01:32:57 +0900

    <FEAT> getCelebrity method in the CelebrityCubic

--- client/lib/domains/doit/presentation/cubits/celebrity_cubic/celebrity_cubic.dart
+++ client/lib/domains/doit/presentation/cubits/celebrity_cubic/celebrity_cubic.dart
@@ -1,8 +1,17 @@
+import 'package:doit_fluttter_study/domains/doit/domain/model/entities/entities.dart';
+import 'package:doit_fluttter_study/domains/doit/domain/services/services.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'celebrity_state.dart';
 
 class CelebrityCubic extends Cubit<CelebrityCubicState> {
-  CelebrityCubic() : super(CelebrityCubicInit());
+  final CelebrityService _celebrityService;
+
+  CelebrityCubic({required CelebrityService celebrityService})
+      : _celebrityService = celebrityService,
+        super(CelebrityCubicInit());
+
+  Future<Iterable<Celebrity>> getCelebrity() async =>
+      await _celebrityService.getCelebrity();
 }
