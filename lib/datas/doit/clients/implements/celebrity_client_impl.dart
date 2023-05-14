8f7687879104d2b53adc5fe3d6eecdcc6403c379
Long9725 <develop9725@gmail.com>
2023-05-15 00:14:55 +0900

    <FEAT> CelebrityClientImpl

---
+++ lib/datas/doit/clients/implements/celebrity_client_impl.dart
@@ -0,0 +1,10 @@
+import 'package:doit_fluttter_study/datas/doit/clients/interfaces/celebrity_client.dart';
+import 'package:doit_fluttter_study/domains/doit/domain/model/dtos/celebrity_dto.dart';
+
+class CelebrityClientImpl implements CelebrityClient {
+  @override
+  Future<Iterable<CelebrityDto>> getCelebrity() {
+    // TODO: implement getCelebrity
+    throw UnimplementedError();
+  }
+}
