960acc2920bea6ea734b1609063737e2a9143806
Long9725 <develop9725@gmail.com>
2023-05-15 00:19:43 +0900

    <FEAT> CelebrityClient getCelebrity method implementation

--- lib/datas/doit/clients/implements/celebrity_client_impl.dart
+++ lib/datas/doit/clients/implements/celebrity_client_impl.dart
@@ -1,10 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:doit_fluttter_study/datas/doit/clients/interfaces/celebrity_client.dart';
 import 'package:doit_fluttter_study/domains/doit/domain/model/dtos/celebrity_dto.dart';
 
 class CelebrityClientImpl implements CelebrityClient {
   @override
-  Future<Iterable<CelebrityDto>> getCelebrity() {
-    // TODO: implement getCelebrity
-    throw UnimplementedError();
+  Future<Iterable<CelebrityDto>> getCelebrity() async {
+    final Uri url = Uri.http('localhost:3000', 'datas');
+    final http.Response response = await http.get(url);
+
+    if (response.statusCode < 300) {
+      final decodedResponse = jsonDecode(response.body) as List<Map>;
+      return decodedResponse.map((e) => CelebrityDto.fromJson(e));
+    }
+    if (response.statusCode < 500) {
+      throw ClientException();
+    }
+    throw ServerException();
   }
 }
