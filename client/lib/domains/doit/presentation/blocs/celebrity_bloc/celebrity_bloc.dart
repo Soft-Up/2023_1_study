84b60c1068ffcea4ad7ef37c87f0239b41afeac4
Long9725 <develop9725@gmail.com>
2023-05-19 14:00:39 +0900

    <FIX> static timer in the debounce sequential

--- client/lib/domains/doit/presentation/blocs/celebrity_bloc/celebrity_bloc.dart
+++ client/lib/domains/doit/presentation/blocs/celebrity_bloc/celebrity_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:doit_fluttter_study/domains/doit/domain/model/entities/entities.dart';
 import 'package:doit_fluttter_study/domains/doit/domain/services/services.dart';
@@ -11,23 +10,23 @@ part 'celebrity_event.dart';
 part 'celebrity_state.dart';
 
 extension Debounce<T> on Stream<T> {
-  static Timer? _timer;
+  Stream<T> debounce(Duration duration) {
+    Timer? timer;
 
-  Stream<T> debounce(Duration duration) =>
-      transform(StreamTransformer.fromHandlers(handleData: (event, sink) {
-        if (_timer?.isActive ?? false) {
-          _timer?.cancel();
-        }
+    return transform(StreamTransformer.fromHandlers(handleData: (event, sink) {
+      if (timer?.isActive ?? false) {
+        timer?.cancel();
+      }
 
-        _timer = Timer(const Duration(milliseconds: 250), () {
-          sink.add(event);
-        });
-      }));
+      timer = Timer(const Duration(milliseconds: 250), () {
+        sink.add(event);
+      });
+    }));
+  }
 }
 
 EventTransformer<Event> debounceSequential<Event>(Duration duration) =>
-    (events, mapper) =>
-        events.debounce(const Duration(milliseconds: 250)).asyncExpand(mapper);
+    (events, mapper) => events.debounce(duration).asyncExpand(mapper);
 
 class CelebrityBloc extends Bloc<CelebrityBlocEvent, CelebrityBlocState> {
   final CelebrityService _celebrityService;
@@ -36,8 +35,8 @@ class CelebrityBloc extends Bloc<CelebrityBlocEvent, CelebrityBlocState> {
       : _celebrityService = celebrityService,
         super(CelebrityBlocInit()) {
     on<RefreshCelebrity>(_onRefresh,
-        transformer: (events, mapper) =>
-            events.debounce(const Duration(milliseconds: 250)));
+        transformer: debounceSequential<RefreshCelebrity>(
+            const Duration(milliseconds: 250)));
     on<ReadNextCelebrity>(_onReadNext);
   }
 
